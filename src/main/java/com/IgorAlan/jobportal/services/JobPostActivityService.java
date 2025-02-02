c377295c2485c07692c3406aadef5a21a04248c5
igor albuquerque <igoralbuquerque102@hotmail.com>
2025-02-02 14:36:41 -0300

    Feito a parte de listar os trabalhos de um recrutador e a parte de um recrutador editar um trabalho

--- src/main/java/com/IgorAlan/jobportal/services/JobPostActivityService.java
+++ src/main/java/com/IgorAlan/jobportal/services/JobPostActivityService.java
@@ -1,10 +1,13 @@
 package com.IgorAlan.jobportal.services;
 
 
-import com.IgorAlan.jobportal.entity.JobPostActivity;
+import com.IgorAlan.jobportal.entity.*;
 import com.IgorAlan.jobportal.repository.JobPostActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class JobPostActivityService {
@@ -18,5 +21,22 @@ public class JobPostActivityService {
 
     public JobPostActivity addNew(JobPostActivity jobPostActivity) {
         return jobPostActivityRepository.save(jobPostActivity);
+    }
+
+    public List<RecruiterJobsDto> getRecruiterJobs(int recruiter){
+        List<IRecruiterJobs> recruiterJobsDtos = jobPostActivityRepository.getRecruiterJobs(recruiter);
+
+        List<RecruiterJobsDto> recruiterJobsDtosList = new ArrayList<>();
+
+        for(IRecruiterJobs rec : recruiterJobsDtos){
+            JobLocation loc = new JobLocation(rec.getState(), rec.getLocationId(), rec.getCity(), rec.getCountry());
+            JobCompany company = new JobCompany("", rec.getName(), rec.getCompanyId());
+            recruiterJobsDtosList.add(new RecruiterJobsDto(company, loc, rec.getJob_title(), rec.getJob_post_id(), rec.getTotalCandidates()));
+        }
+        return recruiterJobsDtosList;
+    }
+
+    public JobPostActivity getOne(int id) {
+        return jobPostActivityRepository.findById(id).orElseThrow(() -> new RuntimeException("Job Post Activity Not Found"));
     }
 }
