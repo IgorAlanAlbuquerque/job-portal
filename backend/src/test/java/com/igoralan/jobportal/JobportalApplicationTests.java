2a85dc60358588c92db4990cb338e4b00879cb0c
IgorAlanAlbuquerque <igoralbuquerque102@hotmail.com>
2025-09-02 15:28:28 -0300

    fix(CI): :bug: Adicionei a tag de integração para o teste da main para rodar no job de integração

--- backend/src/test/java/com/igoralan/jobportal/JobportalApplicationTests.java
+++ backend/src/test/java/com/igoralan/jobportal/JobportalApplicationTests.java
@@ -3,9 +3,11 @@ package com.igoralan.jobportal;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.junit.jupiter.api.Tag;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Tag("integration")
 class JobportalApplicationTests {
 
 	@Test
